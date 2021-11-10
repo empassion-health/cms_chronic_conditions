57c55eab36e4a1c3a58810db9b52dc35892b0343
aneiderhiser <anneiderhiser@gmail.com>
2021-11-09 18:30:39 -0700

    created chronic conditions logic

--- models/stage/stg_encounters.sql
+++ models/stage/stg_encounters.sql
@@ -3,7 +3,7 @@
 
 select
     cast(encounter_id as string) as encounter_id,
-    cast(member_id as string) as member_id,
+    cast(member_id as string) as patient_id,
     to_date(encounter_start_date) as encounter_start_date,
     to_date(encounter_end_date) as encounter_end_date,
     cast(admit_type_code as integer) as admit_type_code,
--- tests/death_after_birth.sql
+++ tests/death_after_birth.sql
@@ -1,6 +1,5 @@
 
 
 select *
-from {{ ref('stg_members') }}
+from {{ ref('stg_patients') }}
 where (deceased_date is not null) and (birth_date > deceased_date)
-
